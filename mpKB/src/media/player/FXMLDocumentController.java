1858923e78c9eaec241f3eb2b222f6e22ff5afdf
TwoDotS2 <09afonso09@gmail.com>
2019-10-10 20:23:43 -0300

    Controlador fxml 1.0

--- mpKB/src/media/player/FXMLDocumentController.java
+++ mpKB/src/media/player/FXMLDocumentController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package media.plaayer;
+package media.player;
 
 import java.awt.Desktop;
 import java.io.File;

bbcff2fc148bb2a211d0afb0ad81895d780e6b13
TwoDotS2 <09afonso09@gmail.com>
2019-10-10 20:22:46 -0300

    Código Media Player 1.0

--- mpKB/src/media/player/MediaPlayer.java
+++ mpKB/src/media/player/MediaPlayer.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package media.plaayer;
+package media.player;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -15,7 +15,7 @@ import javafx.stage.Stage;
  *
  * @author Aluno
  */
-public class MediaPlaayer extends Application {
+public class MediaPlayer extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
